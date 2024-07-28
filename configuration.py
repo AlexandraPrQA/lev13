f3bb7a836ab8ca48886074db31d3bc5ef3cfeb3b
AlexandraPrQA <gra1993@yandex.ru>
2024-07-29 00:22:05 +0500

    diplom

---
+++ configuration.py
@@ -0,0 +1,4 @@
+URL_SERVICE = "https://81732e86-dc8c-47ab-8df2-97e35f16ea9d.serverhub.praktikum-services.ru/"
+#DOC_PATH = "https://59c4947f-f0e2-4b17-a5fa-a447d8738a2d.serverhub.praktikum-services.ru/docs"
+CREATE_NEW_ORDER = "api/v1/orders"
+RECEIPT_ORDER = "api/v1/orders/track?t="
---
+++ data.py
@@ -0,0 +1,16 @@
+
+headers = {"Content-Type": "application/json"}
+
+order_body = {
+    "firstName": "Александра",
+    "lastName": "Перетягина",
+    "address": "Мерлушкина 4",
+    "metroStation": 5,
+    "phone": "+7 987 654 32 10",
+    "rentTime": 2,
+    "deliveryDate": "2024-08-07",
+    "comment": "Без comment",
+    "color": [
+        "BLACK"
+    ]
+}
